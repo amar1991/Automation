cec1db15106b2b290f9511c11849dfc4d95568aa
amar1991 <anitendra@gmail.com>
2025-12-29 17:52:01 +0000

    second push

--- src/test/java/firstTest.java
+++ src/test/java/firstTest.java
@@ -1,10 +1,14 @@
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.io.IOException;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class firstTest {
 
     WebDriver driver;
@@ -12,20 +16,48 @@ public class firstTest {
     @BeforeAll
     public void setUp(){
 
+        WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-
-        driver.get("https://www.google.com/");
+        System.out.println("--Before ALL--");
     }
 
-    @BeforeAll
+    @BeforeEach
+    public void beforeEach(){
+
+        System.out.println("before EACH menthod");
+    }
+
+    @AfterAll
     public void tearDown(){
+
+        System.out.println("Afer ALL menthod");
         driver.quit();
+    }
+
+    @AfterEach
+    public void afterEach(){
+        System.out.println("after EACH nethid");
     }
 
     @Test
     public void openThePage() throws IOException {
-
+        driver.get("https://www.google.com/");
     }
+
+    @DisplayName("USER STORY X - TEST CASE")
+    @Test
+    public void secondMethod() throws IOException {
+        System.out.println("Second Method Run");
+    }
+
+
+    @ParameterizedTest(name = "Run:{index} - value:{arguments}")
+    @ValueSource(ints = {1,2,3})
+    @Test
+    void parameterisedMethod(int theParams) throws IOException {
+        System.out.println("Param Method Run" + theParams);
+    }
+
 
 }
